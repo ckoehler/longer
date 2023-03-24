b7b5399d69ec65954bfc04022dce1521ba291d97
Christoph Koehler <christoph@zerodeviation.net>
2023-03-24 15:34:31 -0600

    refactor: clean up deps and code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 use chrono::Duration;
 use chrono::NaiveDate;
 use clap::Parser;
-// use thiserror::Error;
-// use tracing::*;
-// use date_time_parser::DateParser;
 use prettytable::{row, Table};
-use tracing_subscriber::filter::EnvFilter;
-use tracing_subscriber::fmt;
 
 /// Tool to handle discussion guides
 #[derive(Parser, Debug)]
@@ -17,41 +12,39 @@ struct Opts {
     start: String,
     /// The event to count as the midpoint
     #[arg(short, long)]
-    midpoint: String,
+    event: String,
 }
 
 fn main() {
-    let format = fmt::format().with_target(false);
-
-    // Create a `fmt` collector that uses our custom event format, and set it
-    // as the default.
-    // Also look for RUST_LOG var, but default to INFO level if not set.
-    tracing_subscriber::fmt()
-        .with_env_filter(
-            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info")),
-        )
-        .event_format(format)
-        .init();
-
     // parse command line options
     let opts: Opts = Opts::parse();
-    // parse start date
+
+    // parse start date into year, month, day Vec
     let start: Vec<u32> = opts
         .start
         .split('-')
         .map(|s| s.parse::<u32>().unwrap())
         .collect();
+
+    // parse event date into year, month, day Vec
     let mid: Vec<u32> = opts
-        .midpoint
+        .event
         .split('-')
         .map(|s| s.parse::<u32>().unwrap())
         .collect();
+
+    // now parse those into NaiveDates
     let dt = NaiveDate::from_ymd_opt(start[0].try_into().unwrap(), start[1], start[2]).unwrap();
     let mid = NaiveDate::from_ymd_opt(mid[0].try_into().unwrap(), mid[1], mid[2]).unwrap();
 
+    // grab the duration between Start and Event
     let diff = mid - dt;
 
+    // the "Longer" date will now be Event + diff + 1 day, the first day when you will have been
+    // ____ longer than not.
     let new = mid + diff + Duration::days(1);
+
+    // display the results in a table
     let mut table = Table::new();
     table.add_row(row!["Start date", dt]);
     table.add_row(row!["Event date", mid]);
